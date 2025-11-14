3625c24ab49395254575b82695ab5954822b3d40
Tomas Garcia <15tommgarcia@gmail.com>
2025-11-14 17:58:31 -0300

    Implemento funcionalidad para agregar y mostrar tareas

--- mini-task-manager/app.js
+++ mini-task-manager/app.js
@@ -1,22 +1,50 @@
 // seleccion de elementos del DOM
-const taskInput = document.getElementById("taskInput")
-const addBtn = document.getElementById("addBtn")
-const taskList = document.getElementById("taskList")
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
 
 // array de tareas
-let task = []
+let tasks = [];
 
-// funciones base (TO DO)
-function addTask() {}
-function renderTask() {}
+// funcion para agregar una tarea
+function addTask() {
+    const text = taskInput.value.trim();
+    if (text === "") return; // evita tareas vacías
+
+    const newTask = {
+        id: Date.now(),
+        text,
+        completed: false
+    };
+
+    tasks.push(newTask);
+    renderTasks();
+    taskInput.value = "";
+}
+
+// render de la lista de tareas
+function renderTasks() {
+    taskList.innerHTML = ""
+
+    tasks.forEach( task => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span>${task.text}</span>        
+        `;
+
+        taskList.appendChild(li);
+    });
+}
+
+// To Do
 function toggleTask() {}
 function deleteTask() {}
 
 // evento para el boton
-addBtn.addEventListener("click", addTask)
+addBtn.addEventListener("click", addTask);
 
 // evento para Enter
-taskList.addEventListener("keypress", (e) => {
+taskInput.addEventListener("keypress", (e) => {
     if (e.key === "Enter") addTask();
 })
 
